8a942b9d3500a89f3f4d292f6daca3fdc5b02ead
andreasvidal <andrea.s.vidal00@gmail.com>
2022-06-30 13:18:10 -0400

    change index to main.js

--- hekpers.js
+++ hekpers.js
@@ -1,9 +1,7 @@
-export function getDescription(text) {
-    console.log(text);
- if (text.length > 10) {
-        return text.substring(0, 10) + "...";
-    }
-    console.log(text)
-    return text;
-    
+export const getDescription = (text) => {
+  console.log(text);
+  if (text.length > 10) {
+    return text.substring(0, 10) + "...";
+  }
+  return text;
 }
